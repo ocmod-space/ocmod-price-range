5eaaeee902beb916f7681cb24b6bad827d0c9dea
akaunderr <akaunderr@gmail.com>
2023-10-15 20:33:57 +0300

    fixed packer

--- _pak/reqd.php
+++ _pak/reqd.php
@@ -3,13 +3,13 @@
 // check required tools and php modules
 
 if (!extension_loaded('zip')) {
-	error('php-zip module not installed!');
+	output('php-zip module not installed!');
 }
 
 if (!is_file('/usr/local/bin/hideg')) {
-	error('/usr/local/bin/hideg is missing!');
+	output('/usr/local/bin/hideg is missing!');
 }
 
 if (!is_file('/usr/local/bin/fcl')) {
-	error('/usr/local/bin/fcl is missing!');
+	output('/usr/local/bin/fcl is missing!');
 }
