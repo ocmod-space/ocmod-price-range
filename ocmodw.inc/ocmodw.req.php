e584f88461eba8606e74cca0d7cce6e62b60b1f8
akaunderr <akaunderr@gmail.com>
2024-01-29 13:38:32 +0200

    updated module wrapper

--- ocmodw.inc/ocmodw.req.php
+++ ocmodw.inc/ocmodw.req.php
@@ -3,13 +3,13 @@
 // check required tools and php modules
 
 if (!extension_loaded('zip')) {
-	output('php-zip module not installed!');
+    error('php-zip module not installed!');
 }
 
 if (!is_file('/usr/local/bin/hideg')) {
-	output('/usr/local/bin/hideg is missing!');
+    error('/usr/local/bin/hideg is missing!');
 }
 
 if (!is_file('/usr/local/bin/fcl')) {
-	output('/usr/local/bin/fcl is missing!');
+    error('/usr/local/bin/fcl is missing!');
 }
